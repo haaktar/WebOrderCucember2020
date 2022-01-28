7a516dbce3fd72fb24e61e89f600e448eef6e9c3
haaktar <haaktar@gmail.com>
2022-01-28 14:10:41 -0500

    hakan

--- src/test/java/UploadPractice.java
+++ src/test/java/UploadPractice.java
@@ -1,2 +1,22 @@
-public class UploadPratice{
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class UploadPractice {
+
+    WebDriver driver;
+
+    public void uploadFile() {
+
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.get("http://practice.cybertekschool.com/upload");
+        driver.manage().window().maximize();
+
+        String path = "/Users/hakan/Downloads/Evde Kimse Yok - Ahmet Büke ( PDFDrive ) (1).pdf";
+        WebElement uploadInput = driver.findElement(By.id("file-upload"));
+
+        uploadInput.sendKeys(path);
+
+
+    }
 }
