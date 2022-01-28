21eb586766307723ab422dd37aa1aff78ac92b72
haaktar <haaktar@gmail.com>
2022-01-28 14:17:44 -0500

    hakanj

--- src/test/java/UploadPractice.java
+++ src/test/java/UploadPractice.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class UploadPractice {
 
     WebDriver driver;
 
+    @Test
     public void uploadFile() {
 
         driver = WebDriverFactory.getDriver("Chrome");
@@ -14,8 +16,10 @@ public class UploadPractice {
 
         String path = "/Users/hakan/Downloads/Evde Kimse Yok - Ahmet Büke ( PDFDrive ) (1).pdf";
         WebElement uploadInput = driver.findElement(By.id("file-upload"));
-
         uploadInput.sendKeys(path);
+
+        WebElement uploadButton = driver.findElement(By.id("file-submit"));
+        uploadButton.click();
 
 
     }
